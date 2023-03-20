7966c349b5bc3d31855e59a4e23ba94183942a5b
514660094@qq.com <514660094@qq.com>
2023-03-20 12:34:24 +0800

    1

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const {
   createProxyMiddleware
 } = require('http-proxy-middleware');
 const app = express()
-const port = 9000
+const port = 3000
 
 app.use('/', createProxyMiddleware({
   target: 'https://api.openai.com',
