6359aae7b86c314208fb0c73040eec54138d9ceb
514660094@qq.com <514660094@qq.com>
2023-03-20 13:15:35 +0800

    11

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const {
   createProxyMiddleware
 } = require('http-proxy-middleware');
 const app = express()
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 console.log(port)
 app.use('/test', createProxyMiddleware({
