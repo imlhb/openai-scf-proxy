9601f94a3b73e0e61883583f2bda71e5409c26bc
514660094@qq.com <514660094@qq.com>
2023-03-20 13:20:52 +0800

    11

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const app = express();
 const product = require("./api/product");
+const {
+    createProxyMiddleware
+} = require('http-proxy-middleware');
 
 app.use(express.json({ extended: false }));
 
 app.use("/api/product", product);
+
+app.use('/', createProxyMiddleware({
+    target: 'https://api.openai.com',
+    changeOrigin: true,
+    onProxyRes: function (proxyRes, req, res) {
+        proxyRes.headers['Access-Control-Allow-Origin'] = '*';
+    }
+}));
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
