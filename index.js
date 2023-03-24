dcd678bd49ff0ffb4e40bfdb9bbf7f207382ce17
514660094@qq.com <514660094@qq.com>
2023-03-24 16:33:15 +0800

    1111

--- index.js
+++ index.js
@@ -12,8 +12,12 @@ const {
 app.use('/', createProxyMiddleware({
     target: 'https://api.openai.com',
     changeOrigin: true,
+    buffer: false, // 禁用缓冲区
+    autoRewrite: true, // 自动重写 Location 和 Content-Location 字段
+    headers: { Connection: 'keep-alive' }, // 保持长连接
     onProxyRes: function (proxyRes, req, res) {
         proxyRes.headers['Access-Control-Allow-Origin'] = '*';
+
         // 使用流式传输
         proxyRes.pipe(res);
     }
